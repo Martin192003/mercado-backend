74d389ea43df6615ba551459a2671d64118dd94e
MartinGallardoB <mgallardobertuzzi@itba.edu.ar>
2025-04-18 01:42:36 -0300

    cambioss

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-const mercadopago = require('mercadopago');
+import * as mercadopago from 'mercadopago';
 
 dotenv.config();
 
