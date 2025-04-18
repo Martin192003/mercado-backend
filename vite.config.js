a7a9b208a6d35b765c5b4bc4f456f63491b96c98
MartinGallardoB <mgallardobertuzzi@itba.edu.ar>
2025-04-18 01:31:28 -0300

    eliminé algo

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  root: 'frontend',  // Directorio donde está tu aplicación frontend (ajusta esto según tu estructura)
-  build: {
-    outDir: '../dist', // Directorio donde se guardará la build (ajusta esto según tu estructura)
-  },
-});
