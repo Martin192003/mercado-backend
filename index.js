6b84f26e6bd56567710db03e5a0512591f02c5c9
MartinGallardoB <mgallardobertuzzi@itba.edu.ar>
2025-04-18 01:39:22 -0300

    cambio con require

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import mercadopago from 'mercadopago';
+const mercadopago = require('mercadopago');
 
 dotenv.config();
 
